09958abbf057993d0894b2368a8121f9e3cb71ae
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-01-19 01:36:04 +0000

    Start a Cargo workspace with libraries

---
+++ engine/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn engine_version() -> &'static str {
+    // If we can't determine the version from git tags, fall back to the version
+    // specified in the Cargo manifest
+    let version = git_version::git_version!(cargo_prefix = "cargo:v");
+
+    // If we used Cargo's manifest, adjust the format so it matches the Git version tag format
+    if version.starts_with("cargo:") {
+        return version
+            .strip_prefix("cargo:")
+            .unwrap()
+            .strip_suffix(".0")
+            .unwrap();
+    }
+
+    // Otherwise, if we got a version from Git, we can use it directly
+    version
+}
