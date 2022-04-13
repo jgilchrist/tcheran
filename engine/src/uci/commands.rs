6d84fb26414f98c4470435db4aab9fd6df1265d9
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-13 17:29:39 +0100

    Define UciCommand and UciResponse enums
    These cover all of the possible commands we might receive via stdin and
    all of the responses we might want to send back.
    
    Some of the types are loosely defined since the 'proper' types (e.g.
    Move) are not yet implemented.
    
    For some commands/responses the types aren't clear so those have been
    left as `()` with TODOs.

---
+++ engine/src/uci/commands.rs
@@ -0,0 +1,48 @@
+enum Position {
+    Fen(String),
+    StartPos,
+}
+
+// TODO: Tighten up these types. Position.moves can be Vec<Move> for example.
+enum UciCommand {
+    Uci,
+    Debug {
+        on: bool,
+    },
+    IsReady,
+    SetOption {
+        name: String,
+        value: String,
+    },
+    Register {
+        later: bool,
+        name: String,
+        code: String,
+    },
+    UciNewGame,
+    Position {
+        position: Position,
+        moves: Vec<String>,
+    },
+    Go {
+        searchmoves: Vec<String>,
+        ponder: bool,
+        // TODO
+        wtime: (),
+        btime: (),
+        // TODO
+        winc: (),
+        binc: (),
+        // TODO
+        movestogo: (),
+        depth: u32,
+        nodes: u32,
+        mate: u32,
+        // TODO
+        movetime: (),
+        infinite: bool,
+    },
+    Stop,
+    PonderHit,
+    Quit,
+}
