b07857063765708fe9211d36e1c9fdf7f89e62a8
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-14 00:36:20 +0100

    Implement UCI parsing using nom
    Included commit messages:
    
    Parse UCI commands more strictly
    
    * Don't accept `debugfalse` - require a space between the command and
      arguments (multispace0 vs. multispace1).
    
    * Don't accept arbitrary extra text after single line commands, e.g.
      `uciabcdef` no longer parses to `UciCommand::Uci`. This is technically
      against spec (which says to ignore extra input) so this might have to
      be relaxed again in the future.
    
    Simplify UCI parser for basic commands
    
    * Move eof checking to the main command function so it doesn't have to
      be checked for every individual command.
    
    * Use `map()` to remove the need for `let` -> `Ok` pattern
    
    Remove old parser code
    
    Implement parsing for `go`

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,4 +1,17 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use chess::{
+    board::Board,
+    r#move::Move,
+    square::{File, Rank},
+};
+
 pub mod uci;
+
+fn run(board: &Board) -> Move {
+    Move::new(
+        chess::square::Square(File::E, Rank::R7),
+        chess::square::Square(File::E, Rank::R5),
+    )
+}
