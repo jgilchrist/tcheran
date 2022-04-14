396d00afe8cdac9de0a783b5f49eb1588e4bbe98
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-14 14:28:27 +0100

    Add `Square` constants for convenience (e.g. Square::E4)

--- chess/src/square.rs
+++ chess/src/square.rs
@@ -123,6 +123,79 @@ impl Square {
     pub fn notation(&self) -> String {
         format!("{}{}", self.0, self.1)
     }
+
+    // For convenience
+    pub const A1: Square = Square(File::A, Rank::R1);
+    pub const A2: Square = Square(File::A, Rank::R2);
+    pub const A3: Square = Square(File::A, Rank::R3);
+    pub const A4: Square = Square(File::A, Rank::R4);
+    pub const A5: Square = Square(File::A, Rank::R5);
+    pub const A6: Square = Square(File::A, Rank::R6);
+    pub const A7: Square = Square(File::A, Rank::R7);
+    pub const A8: Square = Square(File::A, Rank::R8);
+
+    pub const B1: Square = Square(File::B, Rank::R1);
+    pub const B2: Square = Square(File::B, Rank::R2);
+    pub const B3: Square = Square(File::B, Rank::R3);
+    pub const B4: Square = Square(File::B, Rank::R4);
+    pub const B5: Square = Square(File::B, Rank::R5);
+    pub const B6: Square = Square(File::B, Rank::R6);
+    pub const B7: Square = Square(File::B, Rank::R7);
+    pub const B8: Square = Square(File::B, Rank::R8);
+
+    pub const C1: Square = Square(File::C, Rank::R1);
+    pub const C2: Square = Square(File::C, Rank::R2);
+    pub const C3: Square = Square(File::C, Rank::R3);
+    pub const C4: Square = Square(File::C, Rank::R4);
+    pub const C5: Square = Square(File::C, Rank::R5);
+    pub const C6: Square = Square(File::C, Rank::R6);
+    pub const C7: Square = Square(File::C, Rank::R7);
+    pub const C8: Square = Square(File::C, Rank::R8);
+
+    pub const D1: Square = Square(File::D, Rank::R1);
+    pub const D2: Square = Square(File::D, Rank::R2);
+    pub const D3: Square = Square(File::D, Rank::R3);
+    pub const D4: Square = Square(File::D, Rank::R4);
+    pub const D5: Square = Square(File::D, Rank::R5);
+    pub const D6: Square = Square(File::D, Rank::R6);
+    pub const D7: Square = Square(File::D, Rank::R7);
+    pub const D8: Square = Square(File::D, Rank::R8);
+
+    pub const E1: Square = Square(File::E, Rank::R1);
+    pub const E2: Square = Square(File::E, Rank::R2);
+    pub const E3: Square = Square(File::E, Rank::R3);
+    pub const E4: Square = Square(File::E, Rank::R4);
+    pub const E5: Square = Square(File::E, Rank::R5);
+    pub const E6: Square = Square(File::E, Rank::R6);
+    pub const E7: Square = Square(File::E, Rank::R7);
+    pub const E8: Square = Square(File::E, Rank::R8);
+
+    pub const F1: Square = Square(File::F, Rank::R1);
+    pub const F2: Square = Square(File::F, Rank::R2);
+    pub const F3: Square = Square(File::F, Rank::R3);
+    pub const F4: Square = Square(File::F, Rank::R4);
+    pub const F5: Square = Square(File::F, Rank::R5);
+    pub const F6: Square = Square(File::F, Rank::R6);
+    pub const F7: Square = Square(File::F, Rank::R7);
+    pub const F8: Square = Square(File::F, Rank::R8);
+
+    pub const G1: Square = Square(File::G, Rank::R1);
+    pub const G2: Square = Square(File::G, Rank::R2);
+    pub const G3: Square = Square(File::G, Rank::R3);
+    pub const G4: Square = Square(File::G, Rank::R4);
+    pub const G5: Square = Square(File::G, Rank::R5);
+    pub const G6: Square = Square(File::G, Rank::R6);
+    pub const G7: Square = Square(File::G, Rank::R7);
+    pub const G8: Square = Square(File::G, Rank::R8);
+
+    pub const H1: Square = Square(File::H, Rank::R1);
+    pub const H2: Square = Square(File::H, Rank::R2);
+    pub const H3: Square = Square(File::H, Rank::R3);
+    pub const H4: Square = Square(File::H, Rank::R4);
+    pub const H5: Square = Square(File::H, Rank::R5);
+    pub const H6: Square = Square(File::H, Rank::R6);
+    pub const H7: Square = Square(File::H, Rank::R7);
+    pub const H8: Square = Square(File::H, Rank::R8);
 }
 
 impl std::fmt::Debug for Square {
--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,11 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use chess::{
-    board::Board,
-    r#move::Move,
-    square::{File, Rank},
-};
+use chess::{board::Board, r#move::Move, square::Square};
 
 pub mod uci;
 
@@ -28,8 +24,5 @@ pub fn engine_version() -> &'static str {
 }
 
 fn run(board: &Board) -> Move {
-    Move::new(
-        chess::square::Square(File::E, Rank::R7),
-        chess::square::Square(File::E, Rank::R5),
-    )
+    Move::new(Square::E7, Square::E5)
 }
