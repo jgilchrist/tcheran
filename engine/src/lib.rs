4dd046611a26225867409140f97cf67acb8ef9de
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-25 20:58:20 +0100

    Add `.pseudo_legal_moves()` and `.legal_moves()` to `Game`

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,4 +1,4 @@
-use chess::{game::Game, movegen::generate_moves, r#move::Move};
+use chess::{game::Game, r#move::Move};
 use rand::prelude::SliceRandom;
 
 pub mod uci;
@@ -22,15 +22,8 @@ pub fn engine_version() -> &'static str {
 }
 
 fn run(game: &Game) -> Move {
-    let moves = generate_moves(game);
-
-    let legal_moves: Vec<Move> = moves
-        .into_iter()
-        .filter(|m| {
-            let next_state = game.make_move(m).unwrap();
-            !next_state.king_in_check(&game.player)
-        })
-        .collect();
-
-    *legal_moves.choose(&mut rand::thread_rng()).unwrap()
+    *game
+        .legal_moves()
+        .choose(&mut rand::thread_rng())
+        .unwrap()
 }
