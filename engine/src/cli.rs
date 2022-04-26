3b5d24cb8b3cd048125900530d6618ec704f9ce4
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-26 15:19:56 +0100

    Add perft and perft-div CLI commands

--- engine/src/cli.rs
+++ engine/src/cli.rs
@@ -1,3 +1,4 @@
+use chess::game::Game;
 use clap::{Parser, Subcommand};
 
 use crate::RunMode;
@@ -16,6 +17,12 @@ enum Commands {
 
     /// Print the starting chessboard
     PrintBoard {},
+
+    /// Run a perft test
+    Perft { depth: u8, fen: String },
+
+    /// Run a perft test for root moves
+    PerftDiv { depth: u8, fen: String },
 }
 
 pub fn parse_cli() -> RunMode {
@@ -25,6 +32,10 @@ pub fn parse_cli() -> RunMode {
         Some(cmd) => match cmd {
             Commands::Uci {} => RunMode::Uci,
             Commands::PrintBoard {} => RunMode::PrintBoard,
+            Commands::Perft { depth, fen } => RunMode::Perft(*depth, Game::from_fen(fen).unwrap()),
+            Commands::PerftDiv { depth, fen } => {
+                RunMode::PerftDiv(*depth, Game::from_fen(fen).unwrap())
+            }
         },
         None => RunMode::default(),
     }
