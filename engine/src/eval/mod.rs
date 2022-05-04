30f0d97c53d3d0a3463d65e7903c4f99b332eb0c
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-05-04 13:26:07 +0100

    Add an Eval struct to represent the result of an evaluation

--- engine/src/eval/mod.rs
+++ engine/src/eval/mod.rs
@@ -1,32 +1,59 @@
-use chess::game::Game;
+use chess::{game::Game, board::PlayerPieces};
 
-const PAWN_VALUE: u64 = 100;
-const KNIGHT_VALUE: u64 = 300;
-const BISHOP_VALUE: u64 = 300;
-const ROOK_VALUE: u64 = 500;
-const QUEEN_VALUE: u64 = 800;
+const PAWN_VALUE: i64 = 100;
+const KNIGHT_VALUE: i64 = 300;
+const BISHOP_VALUE: i64 = 300;
+const ROOK_VALUE: i64 = 500;
+const QUEEN_VALUE: i64 = 800;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Eval(i64);
+
+impl std::ops::Add for Eval {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0)
+    }
+}
+
+impl std::ops::Sub for Eval {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0)
+    }
+}
+
+impl std::ops::Neg for Eval {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
+    }
+}
 
 #[allow(clippy::cast_possible_wrap)]
-pub fn eval(game: &Game) -> i64 {
-    white_piece_value(game) as i64 - black_piece_value(game) as i64
+pub fn eval(game: &Game) -> Eval {
+    white_piece_value(game)
+      + black_piece_value(game)
 }
 
-fn white_piece_value(game: &Game) -> u64 {
-    let pieces = &game.board.white_pieces;
-
-    u64::from(pieces.pawns.count()) * PAWN_VALUE
-        + u64::from(pieces.knights.count()) * KNIGHT_VALUE
-        + u64::from(pieces.bishops.count()) * BISHOP_VALUE
-        + u64::from(pieces.rooks.count()) * ROOK_VALUE
-        + u64::from(pieces.queens.count()) * QUEEN_VALUE
+fn white_piece_value(game: &Game) -> Eval {
+    count_piece_values(&game.board.white_pieces)
 }
 
-fn black_piece_value(game: &Game) -> u64 {
-    let pieces = &game.board.black_pieces;
+fn black_piece_value(game: &Game) -> Eval {
+    -count_piece_values(&game.board.black_pieces)
+}
 
-    u64::from(pieces.pawns.count()) * PAWN_VALUE
-        + u64::from(pieces.knights.count()) * KNIGHT_VALUE
-        + u64::from(pieces.bishops.count()) * BISHOP_VALUE
-        + u64::from(pieces.rooks.count()) * ROOK_VALUE
-        + u64::from(pieces.queens.count()) * QUEEN_VALUE
+fn count_piece_values(pieces: &PlayerPieces) -> Eval
+{
+    Eval(
+        i64::from(pieces.pawns.count()) * PAWN_VALUE
+            + i64::from(pieces.knights.count()) * KNIGHT_VALUE
+            + i64::from(pieces.bishops.count()) * BISHOP_VALUE
+            + i64::from(pieces.rooks.count()) * ROOK_VALUE
+            + i64::from(pieces.queens.count()) * QUEEN_VALUE
+    )
 }
