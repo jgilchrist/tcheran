872333628f4660832d9fd716610e01e183752a45
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2023-03-08 19:32:08 +0000

    Reduce extra work in attacks.rs

--- chess/src/attacks.rs
+++ chess/src/attacks.rs
@@ -1,38 +1,35 @@
-use crate::{
-    board::Board, move_tables, piece::PieceKind, player::Player, square::Square, squares::Squares,
-};
-
-// PERF: There are less naive approaches which would improve performance here
+use crate::{board::Board, move_tables, player::Player, squares::Squares};
 
 pub fn generate_all_attacks(board: &Board, player: Player) -> Squares {
     let mut attacks = Squares::none();
 
-    let our_pieces = board.player_pieces(player).all();
+    let our_pieces = board.player_pieces(player);
     let their_pieces = board.player_pieces(player.other()).all();
-    let all_pieces = our_pieces | their_pieces;
+    let all_pieces = our_pieces.all() | their_pieces;
 
-    for square in our_pieces {
-        let piece_kind = board
-            .player_piece_at(player, square)
-            .expect("Unable to find piece on square it should be on.");
-        attacks |= generate_piece_attacks(piece_kind, player, square, all_pieces);
+    for pawn in our_pieces.pawns {
+        attacks |= move_tables::pawn_attacks(pawn, player);
+    }
+
+    for knight in our_pieces.knights {
+        attacks |= move_tables::knight_attacks(knight);
+    }
+
+    for bishop in our_pieces.bishops {
+        attacks |= move_tables::bishop_attacks(bishop, all_pieces);
+    }
+
+    for rook in our_pieces.rooks {
+        attacks |= move_tables::rook_attacks(rook, all_pieces);
+    }
+
+    for queen in our_pieces.queens {
+        attacks |= move_tables::queen_attacks(queen, all_pieces);
+    }
+
+    for king in our_pieces.king {
+        attacks |= move_tables::king_attacks(king);
     }
 
     attacks
-}
-
-fn generate_piece_attacks(
-    piece_kind: PieceKind,
-    player: Player,
-    square: Square,
-    pieces: Squares,
-) -> Squares {
-    match piece_kind {
-        PieceKind::Pawn => move_tables::pawn_attacks(square, player),
-        PieceKind::Knight => move_tables::knight_attacks(square),
-        PieceKind::Bishop => move_tables::bishop_attacks(square, pieces),
-        PieceKind::Rook => move_tables::rook_attacks(square, pieces),
-        PieceKind::Queen => move_tables::queen_attacks(square, pieces),
-        PieceKind::King => move_tables::king_attacks(square),
-    }
 }
