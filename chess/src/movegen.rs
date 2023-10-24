8b71b4e8d578f8dc636812b78d5af9756c708383
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2023-10-24 22:03:54 +0100

    Add a PERF comment after profiling

--- chess/src/movegen.rs
+++ chess/src/movegen.rs
@@ -53,6 +53,7 @@ pub fn generate_all_attacks(board: &Board, player: Player) -> Squares {
 pub fn generate_moves(game: &Game) -> Vec<Move> {
     let ctx = get_ctx(game);
 
+    // PERF: Allocate this Vec with an initial capacity
     let mut moves: Vec<Move> = vec![];
     generate_pawn_moves(&mut moves, game, &ctx);
     generate_knight_moves(&mut moves, game, &ctx);
