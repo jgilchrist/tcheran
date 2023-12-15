0b723de771dd7a8e5e0eecfeed09f4429de89ea8
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2023-12-15 12:40:19 +0000

    Remove now-unused generate_all_attacks
    We only ever generate attackers of a single square, so this is no longer
    necessary.

--- chess/src/movegen/attackers.rs
+++ chess/src/movegen/attackers.rs
@@ -4,36 +4,6 @@ use crate::movegen::tables;
 use crate::player::Player;
 use crate::square::Square;
 
-pub fn generate_all_attacks(board: &Board, player: Player) -> Bitboard {
-    let mut attacks = Bitboard::EMPTY;
-
-    let our_pieces = board.player_pieces(player);
-    let their_pieces = board.player_pieces(player.other()).all();
-    let all_pieces = our_pieces.all() | their_pieces;
-
-    for pawn in our_pieces.pawns() {
-        attacks |= tables::pawn_attacks(pawn, player);
-    }
-
-    for knight in our_pieces.knights() {
-        attacks |= tables::knight_attacks(knight);
-    }
-
-    for bishop in our_pieces.bishops() | our_pieces.queens() {
-        attacks |= tables::bishop_attacks(bishop, all_pieces);
-    }
-
-    for rook in our_pieces.rooks() | our_pieces.queens() {
-        attacks |= tables::rook_attacks(rook, all_pieces);
-    }
-
-    for king in our_pieces.king() {
-        attacks |= tables::king_attacks(king);
-    }
-
-    attacks
-}
-
 pub fn generate_attackers_of(board: &Board, player: Player, square: Square) -> Bitboard {
     let mut attackers = Bitboard::EMPTY;
     let our_pieces = board.player_pieces(player);
