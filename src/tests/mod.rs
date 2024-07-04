da4568952ef810f1466a8e6b73c93ec7aa0d6bb9
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-07-04 15:45:26 +0100

    Move all perft tests into a single file and generate them using a macro
    This prevents needing duplicate definitions for standard perft tests and
    zobrist tests (and down the road, other potential perft tests).

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,3 +1,2 @@
 mod move_tests;
 mod perft_tests;
-mod zobrist_perft_tests;
--- src/tests/perft_tests.rs
+++ src/tests/perft_tests.rs
@@ -1,6 +1,8 @@
 use crate::chess::fen::START_POS;
 use crate::chess::game::Game;
 use crate::chess::perft::perft;
+use crate::engine::transposition_table::{TTOverwriteable, TranspositionTable};
+use paste::paste;
 
 fn test_perft(fen: &str, depth: u8, expected_positions: usize) {
     crate::init();
@@ -10,278 +12,214 @@ fn test_perft(fen: &str, depth: u8, expected_positions: usize) {
     assert_eq!(expected_positions, actual_positions);
 }
 
-#[test]
-fn perft_startpos_1() {
-    test_perft(START_POS, 1, 20);
+#[derive(Debug, Clone)]
+struct PerftTranspositionTableData {
+    nodes: usize,
+    depth: u8,
 }
 
-#[test]
-fn perft_startpos_2() {
-    test_perft(START_POS, 2, 400);
+impl TTOverwriteable for PerftTranspositionTableData {
+    fn should_overwrite_with(&self, _: &Self) -> bool {
+        false
+    }
 }
 
-#[test]
-fn perft_startpos_3() {
-    test_perft(START_POS, 3, 8902);
+type PerftTranspositionTable = TranspositionTable<PerftTranspositionTableData>;
+
+fn tt_perft(depth: u8, game: &mut Game, tt: &mut PerftTranspositionTable) -> usize {
+    if depth == 1 {
+        return game.moves().len();
+    }
+
+    if let Some(tt_data) = tt.get(&game.zobrist) {
+        if tt_data.depth == depth {
+            return tt_data.nodes;
+        }
+    }
+
+    let result = game
+        .moves()
+        .to_vec()
+        .into_iter()
+        .map(|m| {
+            game.make_move(m);
+            let result = tt_perft(depth - 1, game, tt);
+            game.undo_move();
+            result
+        })
+        .sum();
+
+    tt.insert(
+        &game.zobrist,
+        PerftTranspositionTableData {
+            nodes: result,
+            depth,
+        },
+    );
+
+    result
 }
 
-#[test]
-fn perft_startpos_4() {
-    test_perft(START_POS, 4, 197_281);
+fn test_perft_with_tt(fen: &str, depth: u8, expected_positions: usize) {
+    crate::init();
+
+    let mut tt = PerftTranspositionTable::new();
+    tt.resize(256);
+
+    let mut game = Game::from_fen(fen).unwrap();
+    let actual_positions = tt_perft(depth, &mut game, &mut tt);
+
+    assert_eq!(expected_positions, actual_positions);
 }
 
-#[test]
-fn perft_startpos_5() {
-    test_perft(START_POS, 5, 4_865_609);
+macro_rules! perft_position {
+    ($name:ident, $pos:expr, $depth:expr, $nodes:expr) => {
+        paste! {
+            #[test]
+            fn [<perft_ $name>]() {
+                test_perft($pos, $depth, $nodes);
+            }
+
+            #[test]
+            fn [<perft_tt_ $name>]() {
+                test_perft_with_tt($pos, $depth, $nodes);
+            }
+        }
+    };
 }
+
+perft_position!(startpos_1, START_POS, 1, 20);
+perft_position!(startpos_2, START_POS, 2, 400);
+perft_position!(startpos_3, START_POS, 3, 8902);
+perft_position!(startpos_4, START_POS, 4, 197_281);
+perft_position!(startpos_5, START_POS, 5, 4_865_609);
 
 const KIWIPETE_POS: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-
-#[test]
-fn perft_kiwipete_1() {
-    test_perft(KIWIPETE_POS, 1, 48);
-}
-
-#[test]
-fn perft_kiwipete_2() {
-    test_perft(KIWIPETE_POS, 2, 2039);
-}
-
-#[test]
-fn perft_kiwipete_3() {
-    test_perft(KIWIPETE_POS, 3, 97862);
-}
-
-#[test]
-fn perft_kiwipete_4() {
-    test_perft(KIWIPETE_POS, 4, 4_085_603);
-}
-
-#[test]
-fn perft_kiwipete_5() {
-    test_perft(KIWIPETE_POS, 5, 193_690_690);
-}
+perft_position!(kiwipete_1, KIWIPETE_POS, 1, 48);
+perft_position!(kiwipete_2, KIWIPETE_POS, 2, 2039);
+perft_position!(kiwipete_3, KIWIPETE_POS, 3, 97862);
+perft_position!(kiwipete_4, KIWIPETE_POS, 4, 4_085_603);
+perft_position!(kiwipete_5, KIWIPETE_POS, 5, 193_690_690);
 
 const CHESSPROGRAMMING_WIKI_POS3: &str = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
-
-#[test]
-fn perft_chessprogramming_pos3_1() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS3, 1, 14);
-}
-
-#[test]
-fn perft_chessprogramming_pos3_2() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS3, 2, 191);
-}
-
-#[test]
-fn perft_chessprogramming_pos3_3() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS3, 3, 2812);
-}
-
-#[test]
-fn perft_chessprogramming_pos3_4() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS3, 4, 43238);
-}
-
-#[test]
-fn perft_chessprogramming_pos3_5() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS3, 5, 674_624);
-}
+perft_position!(chessprogramming_pos3_1, CHESSPROGRAMMING_WIKI_POS3, 1, 14);
+perft_position!(chessprogramming_pos3_2, CHESSPROGRAMMING_WIKI_POS3, 2, 191);
+perft_position!(chessprogramming_pos3_3, CHESSPROGRAMMING_WIKI_POS3, 3, 2812);
+perft_position!(
+    chessprogramming_pos3_4,
+    CHESSPROGRAMMING_WIKI_POS3,
+    4,
+    43238
+);
+perft_position!(
+    chessprogramming_pos3_5,
+    CHESSPROGRAMMING_WIKI_POS3,
+    5,
+    674_624
+);
 
 const CHESSPROGRAMMING_WIKI_POS4: &str =
     "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
-
-#[test]
-fn perft_chessprogramming_pos4_1() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS4, 1, 6);
-}
-
-#[test]
-fn perft_chessprogramming_pos4_2() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS4, 2, 264);
-}
-
-#[test]
-fn perft_chessprogramming_pos4_3() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS4, 3, 9467);
-}
-
-#[test]
-fn perft_chessprogramming_pos4_4() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS4, 4, 422_333);
-}
-
-#[test]
-fn perft_chessprogramming_pos4_5() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS4, 5, 15_833_292);
-}
+perft_position!(chessprogramming_pos4_1, CHESSPROGRAMMING_WIKI_POS4, 1, 6);
+perft_position!(chessprogramming_pos4_2, CHESSPROGRAMMING_WIKI_POS4, 2, 264);
+perft_position!(chessprogramming_pos4_3, CHESSPROGRAMMING_WIKI_POS4, 3, 9467);
+perft_position!(
+    chessprogramming_pos4_4,
+    CHESSPROGRAMMING_WIKI_POS4,
+    4,
+    422_333
+);
+perft_position!(
+    chessprogramming_pos4_5,
+    CHESSPROGRAMMING_WIKI_POS4,
+    5,
+    15_833_292
+);
 
 const CHESSPROGRAMMING_WIKI_POS5: &str =
     "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
-
-#[test]
-fn perft_chessprogramming_pos5_1() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS5, 1, 44);
-}
-
-#[test]
-fn perft_chessprogramming_pos5_2() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS5, 2, 1486);
-}
-
-#[test]
-fn perft_chessprogramming_pos5_3() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS5, 3, 62379);
-}
-
-#[test]
-fn perft_chessprogramming_pos5_4() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS5, 4, 2_103_487);
-}
-
-#[test]
-fn perft_chessprogramming_pos5_5() {
-    test_perft(CHESSPROGRAMMING_WIKI_POS5, 5, 89_941_194);
-}
+perft_position!(chessprogramming_pos5_1, CHESSPROGRAMMING_WIKI_POS5, 1, 44);
+perft_position!(chessprogramming_pos5_2, CHESSPROGRAMMING_WIKI_POS5, 2, 1486);
+perft_position!(
+    chessprogramming_pos5_3,
+    CHESSPROGRAMMING_WIKI_POS5,
+    3,
+    62379
+);
+perft_position!(
+    chessprogramming_pos5_4,
+    CHESSPROGRAMMING_WIKI_POS5,
+    4,
+    2_103_487
+);
+perft_position!(
+    chessprogramming_pos5_5,
+    CHESSPROGRAMMING_WIKI_POS5,
+    5,
+    89_941_194
+);
 
 // Extra perft tests positions taken from https://gist.github.com/peterellisjones/8c46c28141c162d1d8a0f0badbc9cff9
-
-#[test]
-fn perft_gist_1() {
-    test_perft("r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2", 1, 8);
-}
-
-#[test]
-fn perft_gist_2() {
-    test_perft("8/8/8/2k5/2pP4/8/B7/4K3 b - d3 0 3", 1, 8);
-}
-
-#[test]
-fn perft_gist_3() {
-    test_perft(
-        "r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2",
-        1,
-        19,
-    );
-}
-
-#[test]
-fn perft_gist_4() {
-    test_perft(
-        "r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2",
-        1,
-        5,
-    );
-}
-
-#[test]
-fn perft_gist_5() {
-    test_perft(
-        "2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2",
-        1,
-        44,
-    );
-}
-
-#[test]
-fn perft_gist_6() {
-    test_perft(
-        "rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9",
-        1,
-        39,
-    );
-}
-
-#[test]
-fn perft_gist_7() {
-    test_perft("2r5/3pk3/8/2P5/8/2K5/8/8 w - - 5 4", 1, 9);
-}
-
-#[test]
-fn perft_gist_8() {
-    test_perft(
-        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-        3,
-        62379,
-    );
-}
-
-#[test]
-fn perft_gist_9() {
-    test_perft(
-        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
-        3,
-        89890,
-    );
-}
-
-#[test]
-fn perft_gist_10() {
-    test_perft("3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 6, 1_134_888);
-}
-
-#[test]
-fn perft_gist_11() {
-    test_perft("8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", 6, 1_015_133);
-}
-
-#[test]
-fn perft_gist_12() {
-    test_perft("8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", 6, 1_440_467);
-}
-
-#[test]
-fn perft_gist_13() {
-    test_perft("5k2/8/8/8/8/8/8/4K2R w K - 0 1", 6, 661_072);
-}
-
-#[test]
-fn perft_gist_14() {
-    test_perft("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 803_711);
-}
-
-#[test]
-fn perft_gist_15() {
-    test_perft("r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1", 4, 1_274_206);
-}
-
-#[test]
-fn perft_gist_16() {
-    test_perft("r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1", 4, 1_720_476);
-}
-
-#[test]
-fn perft_gist_17() {
-    test_perft("2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1", 6, 3_821_001);
-}
-
-#[test]
-fn perft_gist_18() {
-    test_perft("8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1", 5, 1_004_658);
-}
-
-#[test]
-fn perft_gist_19() {
-    test_perft("4k3/1P6/8/8/8/8/K7/8 w - - 0 1", 6, 217_342);
-}
-
-#[test]
-fn perft_gist_20() {
-    test_perft("8/P1k5/K7/8/8/8/8/8 w - - 0 1", 6, 92683);
-}
-
-#[test]
-fn perft_gist_21() {
-    test_perft("K1k5/8/P7/8/8/8/8/8 w - - 0 1", 6, 2217);
-}
-
-#[test]
-fn perft_gist_22() {
-    test_perft("8/k1P5/8/1K6/8/8/8/8 w - - 0 1", 7, 567_584);
-}
-
-#[test]
-fn perft_gist_23() {
-    test_perft("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", 4, 23527);
-}
+perft_position!(gist_1, "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2", 1, 8);
+perft_position!(gist_2, "8/8/8/2k5/2pP4/8/B7/4K3 b - d3 0 3", 1, 8);
+perft_position!(
+    gist_3,
+    "r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2",
+    1,
+    19
+);
+perft_position!(
+    gist_4,
+    "r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2",
+    1,
+    5
+);
+perft_position!(
+    gist_5,
+    "2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2",
+    1,
+    44
+);
+perft_position!(
+    gist_6,
+    "rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9",
+    1,
+    39
+);
+perft_position!(gist_7, "2r5/3pk3/8/2P5/8/2K5/8/8 w - - 5 4", 1, 9);
+perft_position!(
+    gist_8,
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+    3,
+    62379
+);
+perft_position!(
+    gist_9,
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+    3,
+    89890
+);
+perft_position!(gist_10, "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 6, 1_134_888);
+perft_position!(gist_11, "8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", 6, 1_015_133);
+perft_position!(gist_12, "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", 6, 1_440_467);
+perft_position!(gist_13, "5k2/8/8/8/8/8/8/4K2R w K - 0 1", 6, 661_072);
+perft_position!(gist_14, "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 803_711);
+perft_position!(
+    gist_15,
+    "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1",
+    4,
+    1_274_206
+);
+perft_position!(
+    gist_16,
+    "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1",
+    4,
+    1_720_476
+);
+perft_position!(gist_17, "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1", 6, 3_821_001);
+perft_position!(gist_18, "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1", 5, 1_004_658);
+perft_position!(gist_19, "4k3/1P6/8/8/8/8/K7/8 w - - 0 1", 6, 217_342);
+perft_position!(gist_20, "8/P1k5/K7/8/8/8/8/8 w - - 0 1", 6, 92683);
+perft_position!(gist_21, "K1k5/8/P7/8/8/8/8/8 w - - 0 1", 6, 2217);
+perft_position!(gist_22, "8/k1P5/8/1K6/8/8/8/8 w - - 0 1", 7, 567_584);
+perft_position!(gist_23, "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", 4, 23527);
