jgilchrist/tcheran

commit 559f24b6ac3ea69aac1b3ce73e9e0d914241456d

Allocate the TT before the first search
This seems to have been the cause of the drop in Elo when we started
initialising the TT before each game. The first search is when we call
.resize() but this can take ~70ms, which for STC SPRT tests can mean the
first move is only searched to depth 1 or 2.

Obviously this can be pretty catastrophic in some situations, so after
the 'fix' we started making more mistakes in the first move out of book.

Elo   | 10.45 +- 7.45 (95%)
SPRT  | 8.0+0.08s Threads=1 Hash=16MB
LLR   | 2.95 (-2.94, 2.94) [-5.00, 0.00]
Games | N: 4090 W: 1501 L: 1378 D: 1211
Penta | [140, 342, 996, 389, 178]

src/engine/search/mod.rs
@@ -54,9 +54,9 @@ pub struct PersistentState {
 }
 
 impl PersistentState {
-    pub fn new() -> Self {
+    pub fn new(tt_size_mb: usize) -> Self {
         Self {
-            tt: SearchTranspositionTable::default(),
+            tt: SearchTranspositionTable::new(tt_size_mb),
             history_table: HistoryTable::new(),
         }
     }

src/engine/transposition_table.rs
@@ -24,11 +24,13 @@ pub fn calculate_number_of_entries<T: Clone + TTOverwriteable>(size_mb: usize) -
 }
 
 impl<T: Clone + TTOverwriteable> TranspositionTable<T> {
-    pub fn new() -> Self {
+    pub fn new(size_mb: usize) -> Self {
+        let number_of_entries = calculate_number_of_entries::<T>(size_mb);
+
         Self {
-            data: vec![None; 0],
+            data: vec![None; number_of_entries],
+            size: size_mb,
             occupied: 0,
-            size: 0,
             generation: 0,
         }
     }
@@ -103,11 +105,5 @@ impl<T: Clone + TTOverwriteable> TranspositionTable<T> {
         }
 
         None
-    }
-}
-
-impl<T: Clone + TTOverwriteable> Default for TranspositionTable<T> {
-    fn default() -> Self {
-        Self::new()
     }
 }

src/engine/uci/mod.rs
@@ -218,7 +218,12 @@ impl Uci {
             UciCommand::IsReady => send_response(&UciResponse::ReadyOk),
             UciCommand::SetOption { name, value } => {
                 match name.as_str() {
-                    options::HashOption::NAME => options::HashOption::set(&mut self.options, value),
+                    options::HashOption::NAME => {
+                        let new_size = options::HashOption::set(&mut self.options, value)?;
+                        let mut tt_handle = self.persistent_state.lock().unwrap();
+                        tt_handle.tt.resize(new_size);
+                        Ok(())
+                    }
                     options::ThreadsOption::NAME => {
                         options::ThreadsOption::set(&mut self.options, value)
                     }
@@ -231,7 +236,8 @@ impl Uci {
             }
             UciCommand::UciNewGame => {
                 self.game = Game::new();
-                self.persistent_state = Arc::new(Mutex::new(PersistentState::new()));
+                self.persistent_state =
+                    Arc::new(Mutex::new(PersistentState::new(self.options.hash_size)));
 
                 self.is_stopped.reset();
             }
@@ -508,6 +514,8 @@ pub enum UciInputMode {
 }
 
 pub fn uci(uci_input_mode: UciInputMode) -> Result<(), String> {
+    let options = EngineOptions::default();
+
     let mut uci = Uci {
         control: None,
         is_stopped: Arc::new(LockLatch::new()),
@@ -515,10 +523,10 @@ pub fn uci(uci_input_mode: UciInputMode) -> Result<(), String> {
             pretty_output: std::io::stdin().is_terminal(),
         },
         debug: false,
-        options: EngineOptions::default(),
+        persistent_state: Arc::new(Mutex::new(PersistentState::new(options.hash_size))),
 
         game: Game::new(),
-        persistent_state: Arc::new(Mutex::new(PersistentState::new())),
+        options,
 
         block_on_threads: match uci_input_mode {
             UciInputMode::Stdin => false,

src/engine/uci/options.rs
@@ -24,8 +24,6 @@ pub enum UciOptionType {
 pub trait UciOption {
     const NAME: &'static str;
     const DEF: UciOptionType;
-
-    fn set(options: &mut EngineOptions, value: &str) -> Result<(), String>;
 }
 
 pub struct HashOption;
@@ -37,12 +35,14 @@ impl UciOption for HashOption {
         min: 0,
         max: 1024,
     };
+}
 
-    fn set(options: &mut EngineOptions, value: &str) -> Result<(), String> {
+impl HashOption {
+    pub fn set(options: &mut EngineOptions, value: &str) -> Result<usize, String> {
         let hash_size = value.parse::<usize>().map_err(|_| "Invalid value")?;
 
         options.hash_size = hash_size;
-        Ok(())
+        Ok(hash_size)
     }
 }
 
@@ -55,8 +55,10 @@ impl UciOption for ThreadsOption {
         min: 1,
         max: 1,
     };
+}
 
-    fn set(options: &mut EngineOptions, value: &str) -> Result<(), String> {
+impl ThreadsOption {
+    pub fn set(options: &mut EngineOptions, value: &str) -> Result<(), String> {
         let threads = value.parse::<usize>().map_err(|_| "Invalid value")?;
 
         options.threads = threads;
@@ -73,8 +75,10 @@ impl UciOption for MoveOverheadOption {
         min: 0,
         max: 1000,
     };
+}
 
-    fn set(options: &mut EngineOptions, value: &str) -> Result<(), String> {
+impl MoveOverheadOption {
+    pub fn set(options: &mut EngineOptions, value: &str) -> Result<(), String> {
         let move_overhead = value.parse::<usize>().map_err(|_| "Invalid value")?;
 
         options.move_overhead = move_overhead;

commit 1c1c961f27f3a16290c5edf8c12eb43816937b90

Fix double allocation of PersistentState
559f24b introduced a potential issue where we allocated a
PersistentState when starting up the engine.

We also reset the PersistentState between games by creating a new
`Arc<Mutex<PersistentState>>`. This meant that after `ucinewgame` was
run, there could very briefly be two `PersistentState` hanging around in
memory.

The problem here is the TT. Although it shouldn't be an issue in most
places, in very memory constrained environments, we might not have
enough room for two TTs to be allocated at the same time. This was
causing the process to be OOM-killed.

The new approach avoids reallocating PersistentState and instead
introduces methods to re-zero everything within it.

src/engine/search/mod.rs
@@ -60,6 +60,11 @@ impl PersistentState {
             history_table: HistoryTable::new(),
         }
     }
+
+    pub fn reset(&mut self) {
+        self.tt.reset();
+        self.history_table.reset();
+    }
 }
 
 pub struct SearchState {

src/engine/search/tables.rs
@@ -42,6 +42,16 @@ impl HistoryTable {
         Self([[[0; Square::N]; Square::N]; Player::N])
     }
 
+    pub fn reset(&mut self) {
+        for from_square in 0..Square::N {
+            for to_square in 0..Square::N {
+                for player in 0..Player::N {
+                    self.0[player][from_square][to_square] = 0;
+                }
+            }
+        }
+    }
+
     fn bonus(depth: u8) -> i32 {
         let depthi32 = i32::from(depth);
 

src/engine/transposition_table.rs
@@ -36,6 +36,15 @@ impl<T: Clone + TTOverwriteable> TranspositionTable<T> {
         tt
     }
 
+    pub fn reset(&mut self) {
+        for i in 0..self.data.len() {
+            self.data[i] = None;
+        }
+
+        self.generation = 0;
+        self.occupied = 0;
+    }
+
     pub fn resize(&mut self, size_mb: usize) {
         if self.size == size_mb {
             return;

src/engine/uci/mod.rs
@@ -236,10 +236,10 @@ impl Uci {
             }
             UciCommand::UciNewGame => {
                 self.game = Game::new();
-                self.persistent_state =
-                    Arc::new(Mutex::new(PersistentState::new(self.options.hash_size)));
-
                 self.is_stopped.reset();
+
+                let mut persistent_state_handle = self.persistent_state.lock().unwrap();
+                persistent_state_handle.reset();
             }
             UciCommand::Position { position, moves } => {
                 let mut game = match position {