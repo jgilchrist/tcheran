56ee05ec91e1d9638246fc09a16868b04888ab1e
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-10-29 21:58:55 +0000

    Capture more details about panics
    We were discarding information about the panic payload. Instead, we now
    capture the payload and insert it into the panic message.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod tests;
 use crate::engine::uci::UciInputMode;
 use engine::uci;
 use engine::util::log;
+use std::panic::PanicHookInfo;
 use std::process::ExitCode;
 
 pub const ENGINE_NAME: &str = "Tcheran";
@@ -14,7 +15,11 @@ pub const ENGINE_NAME: &str = "Tcheran";
 pub fn engine_version() -> String {
     let cargo_version = env!("CARGO_PKG_VERSION");
     let version = cargo_version.strip_suffix(".0").unwrap();
-    let dev_suffix = if cfg!(feature = "release") { "" } else { "-dev" };
+    let dev_suffix = if cfg!(feature = "release") {
+        ""
+    } else {
+        "-dev"
+    };
 
     format!("v{version}{dev_suffix}")
 }
@@ -24,10 +29,22 @@ pub fn init() {
     engine::init();
 }
 
+fn get_panic_message(info: &PanicHookInfo<'_>) -> String {
+    if let Some(s) = info.payload().downcast_ref::<&str>() {
+        format!("panic occurred: {s:?} {info:?}")
+    } else if let Some(s) = info.payload().downcast_ref::<String>() {
+        format!("panic occurred: {s:?} {info:?}")
+    } else {
+        format!("{info:?}")
+    }
+}
+
 fn main() -> ExitCode {
     std::panic::set_hook(Box::new(|info| {
-        println!("{info}");
-        log::crashlog(format!("{info:?}"));
+        let panic_message = get_panic_message(info);
+
+        println!("{panic_message}");
+        log::crashlog(panic_message);
     }));
 
     let args = std::env::args().collect::<Vec<_>>();
