40e6bf685d7cd1e6d8a84c4e4af5b111211d8a47
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-11-24 12:27:20 +0000

    Ensure build.rs works on Windows
    We need .canonicalize to ensure the path ends up as a Windows-compatible
    path.

--- build.rs
+++ build.rs
@@ -1,22 +1,12 @@
-use std::path::PathBuf;
-
 fn main() {
     build_fathom();
 }
 
 fn build_fathom() {
-    let fathom_dir = PathBuf::from("src/engine/tablebases/fathom/src")
-        .canonicalize()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .to_string();
+    println!("cargo:rerun-if-changed=src/engine/tablebases/fathom/src");
 
-    println!("cargo:rerun-if-changed={fathom_dir}");
-
-    let mut cc = cc::Build::new();
-    cc.file(format!("{fathom_dir}/tbprobe.c"));
-    cc.include(fathom_dir);
-
-    cc.compile("fathom");
+    cc::Build::new()
+        .include("src/engine/tablebases/fathom/src")
+        .file("src/engine/tablebases/fathom/src/tbprobe.c")
+        .compile("fathom");
 }
